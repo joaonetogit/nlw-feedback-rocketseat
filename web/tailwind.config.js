bd819b35691bfc2c24829cf238bbaa320bbfcf5b
joaonetopd <joaopinheiro.netos@gmail.com>
2022-06-20 00:11:20 -0300

    web front-end

---
+++ web/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          300: '#996DFF',
+          500: '#8257e6',
+        },
+      },
+      borderRadius: {
+        md: '4px',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
+};
